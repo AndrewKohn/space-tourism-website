ee57bd879eddca34eb0858b794983320a8dd924c
andrewkohn <contact@kohnandrew.com>
2023-10-24 08:18:31 -0500

    Add: Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,23 @@
+import './Home.scss';
+
+const Home = () => {
+  return (
+    <section className="home-container">
+      <div className="home--text">
+        <h5>So, you want to travel to</h5>
+        <h1>SPACE</h1>
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+      <button className="home-btn">
+        <h4 className="home-btn--text">EXPLORE</h4>
+      </button>
+    </section>
+  );
+};
+
+export default Home;
